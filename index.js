4ebaa5ee973443a5a7435b21f8875a75d9ab25ba
brench maina <bein67544@gmail.com>
2025-06-06 23:37:32 +0300

    arithmetic lab done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+ const num1 = 31;
+const num2 = 2;
+const num3 = 16;
+const num4 = 6;
+// equation whose multipliced result is 62
+const multiply = num1* num2;
+// generate  a random integer grater than 0
+const random = Math.floor(Math.random() * 50) + 1;
+// equation whose result is 4
+const mod = num3 % num4;
+// Find the highest number in a set
+const max = Math.max(5, 10, 15, 20);
+
